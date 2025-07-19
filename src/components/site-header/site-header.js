897b760e538ff28f61a5c6e89ca8290fe00ac47f
Ivan Samoylov <vanek.samoylov@yandex.ru>
2025-07-19 23:56:19 +0300

    feature: добавлена верстка хедера для мобильных устройств

--- src/components/site-header/site-header.js
+++ src/components/site-header/site-header.js
@@ -17,12 +17,32 @@ class SiteHeader extends HTMLElement {
     this.shadowRoot.innerHTML = `
       <style>${styles}</style>
       <header class="site-header">
-        <a href="/" class="site-header__logo-container">
-          <img src="" class="site-header__logo-image" id="logo-img" />
-        </a>
-        <nav class="site-header__nav" id="nav"></nav>
-        <div class="site-header__contact" id="contact"></div>
-        <ul class="site-header__buttons-list" id="buttons-list"></ul>
+        <button
+          type="button"
+          class="site-header__side-menu-button site-header__button"
+          id="side-menu-button"
+        >
+          <img src="src/images/icons/side-menu-icon.png" alt="cart-icon" class="site-header__button-icon" />
+        </button>
+        <div class="site-header__nav-container">
+          <a href="/" class="site-header__logo-container">
+            <img src="" class="site-header__logo-image" id="logo-img" />
+          </a>
+          <nav class="site-header__nav" id="nav"></nav>
+        </div>
+        <div class="site-header__utils-container">
+          <div class="site-header__contact" id="contact"></div>
+          <div class="site-header__buttons-container">
+            <ul class="site-header__buttons-list" id="buttons-list"></ul>
+            <button
+              type="button"
+              class="site-header__button"
+              id="cart-button"
+            >
+              <img src="src/images/icons/cart-icon.png" alt="cart-icon" class="site-header__button-icon" />
+            </button>
+          </div>
+        </div>
       </header>
     `;
 
